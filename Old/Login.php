74b53ba4fa935fde597376c6d8aad0cfc97530db
Luke Brouwer <Luke@LMBP.local>
2020-09-14 17:50:43 -0700

    put up

---
+++ Old/Login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in!</title>
+    <link rel="stylesheet" href="Login.css">
+</head>
+<body>
+    <h1>Registration</h1>
+    
+
+    <form class="box" form action="ProcessLogin.php"method="post">
+        <h1 id="#signup">Log in</h1>
+        <input type="text" name="Username" placeholder="Username">
+        <input type="password" name="Password" placeholder="Password">
+        <input type="submit" name="Submit" value="Log in">
+    </form>
+</body>
+</html>
---
+++ Old/Signup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up!</title>
+    <link rel="stylesheet" href="Signup.css">
+</head>
+
+<body>
+   
+    <form class="box" action="ProcessSignup.php" methods="post">
+        <h1 id="#signup">Sign up</H1>
+        <!-- <input type="text" name="Username" placeholder="Username">
+        <input type="password" name="Password" placeholder="Password">
+        <input type="password" name="vPassword" placeholder="Verify Password">
+        <input type="submit" name="Submit" value="Log in"> -->
+        <input type="text" name="Username" placeholder="Username">
+        <input type="password" name="Password" placeholder="Password">
+        <input type="submit" name="Submit" value="Sign in">
+    </form>
+
+</body>
+</html>
